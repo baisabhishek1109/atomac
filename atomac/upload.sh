705ada125c9723b7f17e54246b6c7dad197567eb
abhsing@adobe.com <abhsing@adobe.com>
2017-06-12 18:13:24 +0530

    adding atomac

---
+++ atomac/upload.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# If you did a sudo python setup.py install, you may need to remove some
+# directories first - dist, build, and atomac.egg-info
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+sudo rm -rf $DIR/dist
+sudo rm -rf $DIR/build
+sudo rm -rf $DIR/atomac.egg-info
+
+python setup.py register
+python setup.py sdist upload --sign
+python setup.py bdist_egg upload --sign
+
